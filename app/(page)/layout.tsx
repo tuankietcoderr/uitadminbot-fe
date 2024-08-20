485b017167dcd903fe5f3d0f4a51296ac1ec27d1
Tuan Kiet Tran <charlieismylife2003@gmail.com>
2024-08-20 22:31:04 +0700

    add metadata

--- app/(page)/layout.tsx
+++ app/(page)/layout.tsx
@@ -1,4 +1,17 @@
+import { Metadata, ResolvingMetadata } from "next"
 import { PropsWithChildren } from "react"
+
+export async function generateMetadata({ params, searchParams }: any, parent: ResolvingMetadata): Promise<Metadata> {
+  // optionally access and extend (rather than replace) parent metadata
+  const previousImages = (await parent).openGraph?.images || []
+
+  return {
+    metadataBase: new URL("https://uitadminbot.vercel.app"),
+    openGraph: {
+      images: ["/og_bg.jpg", ...previousImages]
+    }
+  }
+}
 
 const layout = ({ children }: PropsWithChildren) => {
   return children
